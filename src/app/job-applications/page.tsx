9ce4316f7fc1ca1b8bbdd37975db013a7cd75499
Douglas Lessa <douglas.lessa@ecs.com.br>
2024-04-14 18:06:24 -0300

    feat: list jobs to populate table

--- src/app/job-applications/page.tsx
+++ src/app/job-applications/page.tsx
@@ -4,40 +4,57 @@ import { container } from 'tsyringe'
 import { useState } from 'react'
 
 import Layout from '@/app/components/Layout'
+import { JobApplication } from '@/app/core/models/job-application'
+import { JobApplicationService } from '@/app/core/services/job-application-service'
 import Loading from '../components/Loading'
+
+
+const jobApplicationService = container.resolve(JobApplicationService)
 
 export default function Home() {
 
   const [loadingState, setLoadingState] = useState({ show: true })
+  const [jobApplicationsState, setJobApplicationsState] = useState([] as JobApplication[])
+  
+  if (!jobApplicationsState.length) {
+      jobApplicationService.list(0, 10).then((data) => {
+        console.log(data)
+        setJobApplicationsState(data)
+        setLoadingState({ show: false })
+      })
+  }
 
   function renderTableBody() {
-    if (!jobApplications.length) {
-      <tr>
-        <td colSpan={4}>
-          Não há candidatura
-        </td>
-      </tr>
+
+    if (!jobApplicationsState?.length) {
+      return <tr>
+          <td colSpan={4}>
+            Não há candidatura
+          </td>
+        </tr>
+    } else {
+      return jobApplicationsState?.map((jobApplication, i) => (
+        <tr key={jobApplication.id}>
+          <td>
+            {jobApplication.company}
+          </td>
+          <td>
+            {jobApplication.candidateAt.toString().slice(0, 10).split('-').reverse().join('/')}
+          </td>
+          <td>
+            {jobApplication.channel}
+          </td>
+          <td className="flex justify-evenly">
+            <a href={`/job-applications/${jobApplication.id}`}>Ver</a>
+          </td>
+        </tr>
+      ))
     }
-    return jobApplications.map((jobApplication, i) => (
-      <tr>
-        <td>
-          {jobApplication.company}
-        </td>
-        <td>
-          {jobApplication.candidateAt.toISOString().slice(0, 10).split('-').reverse().join('/')}
-        </td>
-        <td>
-          {jobApplication.channel}
-        </td>
-        <td className="flex justify-evenly">
-          <a href={`/job-applications/${jobApplication.id}`}>Ver</a>
-        </td>
-      </tr>
-    ))
+    
   }
   return (
     <>
-    <Layout title="Candidaturas" toGoBack="/">
+      <Layout title="Candidaturas" toGoBack="/">
         <div className='flex justify-end'>
           <a href='/job-applications/new' className='flex align-center px-4 py-2 bg-indigo-900 rounded-md'>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
@@ -46,29 +63,29 @@ export default function Home() {
             Novo
           </a>
         </div>
-      <table className='table-auto w-full bg-slate-300 text-indigo-900 rounded-md border-separate border-spacing-6 text-center'>
-        <thead>
-          <tr>
-            <th className=''>
-              Empresa
-            </th>
-            <th className=''>
-              Candidatado em
-            </th>
-            <th className=''>
-              Canal
-            </th>
-            <th className=''>
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            renderTableBody()
-          }
-        </tbody>
-      </table>
-    </Layout>
+        <table className='table-auto w-full bg-slate-300 text-indigo-900 rounded-md border-separate border-spacing-6 text-center'>
+          <thead>
+            <tr>
+              <th className=''>
+                Empresa
+              </th>
+              <th className=''>
+                Candidatado em
+              </th>
+              <th className=''>
+                Canal
+              </th>
+              <th className=''>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              renderTableBody()
+            }
+          </tbody>
+        </table>
+      </Layout>
       <Loading show={loadingState.show} />
     </>
   );
